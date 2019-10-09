d5901e1826a2eaa40baf442c8210f1f3a0669a8d
anjingwsno1 <46479333+anjingwsno1@users.noreply.github.com>
2019-10-09 16:35:56 +1100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -41,7 +41,16 @@
 
 
     //======= PROGRESS BAR  ========//
-
+		$('.progress-bar-style').each(function() {
+			var progress = $(this).data("progress");
+			var prog_width = progress+'%';
+			if (progress <= 100) {
+				$(this).append('<div class="bar-inner" style="width:'+prog_width+'"><span>'+prog_width+'</span></div>');
+			}else{
+				$(this).append('<div class="bar-inner" style="width:100%"><span>'+prog_width+'</span></div>');
+			}
+		});
+	
 
 	//======= ISOTOP FILTERING JS  ========//
     $(window).on('load', function() { 
